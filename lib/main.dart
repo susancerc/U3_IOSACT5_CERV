c1f0eb22a9b39c66f31eec775c284a812370d60b
susancerc <71361469+susancerc@users.noreply.github.com>
2021-05-27 15:26:35 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class SusiTarjeta extends StatelessWidget {
               title: Text('AppBar Tarjetas de Susi'),
               actions: <Widget>[
                 IconButton(icon: Icon(Icons.add_a_photo), onPressed: () {}),
-              ], // fin de widget[]
+              ], // fin de widget
             ), // fin de scaffold
             body: Container(
                 child: ListView(
@@ -29,10 +29,9 @@ class SusiTarjeta extends StatelessWidget {
                 SizedBox(height: 5.0),
                 _items('https://raw.githubusercontent.com/susancerc/fotosss/main/im5.jpg', 'sushi con nubecitas'),
                 SizedBox(height: 5.0),
-              ], // fin de widget []
+              ], // fin de widget 
             )))); // fin de material app
   } // fin de widget
-
   Widget _items(String url, String producto) {
     return Container(
       padding: EdgeInsets.only(top: 5.0, right: 10.0, left: 10.0),
@@ -50,17 +49,17 @@ class SusiTarjeta extends StatelessWidget {
                     backgroundColor: Colors.indigo[300],
                     backgroundImage: NetworkImage(url),
                     maxRadius: 35.0,
-                  ),
+                  ),//circle avatar y listtile
                   title: Text(producto, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12.0)),
-                  subtitle: Text('SSD DE SUSI', style: TextStyle(fontSize: 9.0)),
+                  subtitle: Text('SSD DE SUSI', style: TextStyle(fontSize: 9.0)),//solid state drive 
                   trailing: Icon(
                     Icons.arrow_forward_ios_rounded,
                     color: Colors.white,
-                  ),
-                ))
-          ],
-        ),
-      ),
-    );
-  }
+                  ),// fin icon
+                ))//fin container
+          ],//fin <widget>
+        ),//fin app bar
+      ),// fin scaffold
+    );// fin material app
+  }// fin widget build
 } // fin de SusiTarjeta
